58ca02f135bba020567766d9f26255f01232a6b4
Mohd Aman <aman@Mohds-MacBook-Pro.local>
2025-07-26 23:01:26 +0530

    udpate

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -9,16 +9,10 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  // appId: process.env.REACT_APP_FIREBASE_APP_ID
   apiKey: "AIzaSyCjMoBKWjIAgey7GpgPB9PI0ZIaFXR7Ldg",
   authDomain: "challane.firebaseapp.com",
   projectId: "challane",
-  storageBucket: "challane.appspot.com",
+  storageBucket: "challane.firebasestorage.app",
   messagingSenderId: "880697432147",
   appId: "1:880697432147:web:bb75326d0bbb97ec56cea6",
 };
